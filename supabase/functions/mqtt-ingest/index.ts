3454eded58cbe88449fdee0298d976bcdd31c9d8
Nithika <nithika.karuna@gmail.com>
2026-04-06 19:50:48 -0400

    add deno

--- supabase/functions/mqtt-ingest/index.ts
+++ supabase/functions/mqtt-ingest/index.ts
@@ -1,4 +1,4 @@
-import { serve } from 'https://deno.land/std@0.177.0/http/server.ts'
+import { serve } from 'https://deno.land/std/http/server.ts'
 
 //hivemq sends post with this frmat
 interface HiveMQWebhookPayload {
